f78e1e44d97910f6c598a81cab6b77b698a16eb4
lyfe00011 <lyfe00011@gmail.com>
2022-04-25 15:40:41 +0530

    add afk

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const client = require('./lib/client')
 const { DATABASE } = require('./config')
 const start = async () => {
 	try {
-		console.log('wa-bot')
+		console.log('whatsapp-bot-md')
 		await DATABASE.sync()
 		console.log('DB syncing')
 		await client.connect()
