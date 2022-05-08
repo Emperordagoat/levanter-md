e050853441c482e47c17bf620243988fa06af9f0
lyfe00011 <lyfe00011@gmail.com>
2022-05-08 15:01:49 +0530

    add fulss

--- plugins/ss.js
+++ plugins/ss.js
@@ -1,16 +1,33 @@
-const { bot, isUrl } = require('../lib/')
+const { bot } = require('../lib/')
 
 bot(
 	{
 		pattern: 'ss ?(.*)',
 		fromMe: true,
-		desc: 'Download pinterest video/image',
+		desc: 'Take web page screenshot',
 		type: 'download',
 	},
 	async (message, match) => {
-		match = isUrl(match || message.reply_message.text)
+		match = match || message.reply_message.text
 		if (!match) return await message.sendMessage('_Example : ss url_')
-		return await message.sendFromUrl(`https://shot.screenshotapi.net/screenshot?&url=${match}
-		&width=1388&height=720&output=image&file_type=png&block_ads=true&no_cookie_banners=true&dark_mode=true&wait_for_event=networkidle`)
+		return await message.sendFromUrl(
+			`https://shot.screenshotapi.net/screenshot?&url=${match}&fresh=true&output=image&file_type=png&block_ads=true&no_cookie_banners=true&destroy_screenshot=true&dark_mode=true&wait_for_event=networkidle`
+		)
+	}
+)
+
+bot(
+	{
+		pattern: 'fullss ?(.*)',
+		fromMe: true,
+		desc: 'Take web page screenshot',
+		type: 'download',
+	},
+	async (message, match) => {
+		match = match || message.reply_message.text
+		if (!match) return await message.sendMessage('_Example : fullsss url_')
+		return await message.sendFromUrl(
+			`https://shot.screenshotapi.net/screenshot?&url=${match}&full_page=true&output=image&file_type=png&block_ads=true&no_cookie_banners=true&destroy_screenshot=true&dark_mode=true&wait_for_event=networkidle`
+		)
 	}
 )
