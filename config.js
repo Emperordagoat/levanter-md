f6958c77f862984d67868275cfaa8db511b2ddb6
lyfe00011 <lyfe00011@gmail.com>
2022-05-21 21:13:00 +0530

    fix

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ const DATABASE_URL =
 		? './database.db'
 		: process.env.DATABASE_URL
 module.exports = {
-	VERSION: '2.2.7',
+	VERSION: '2.2.8',
 	SESSION_ID: process.env.SESSION_ID || '',
 	DATABASE:
 		DATABASE_URL === './database.db'
