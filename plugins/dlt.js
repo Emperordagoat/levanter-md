5a39a5c23f21474b87cbc5c6a808f85368a34e92
lyfe00011 <lyfe00011@gmail.com>
2022-08-01 22:20:42 +0530

    add dlt

---
+++ plugins/dlt.js
@@ -0,0 +1,21 @@
+const { bot, isAdmin } = require('../lib')
+
+bot(
+	{
+		pattern: 'dlt ?(.*)',
+		fromMe: true,
+		desc: 'delete replied msg',
+		type: 'whatsapp',
+	},
+	async (message, match) => {
+		if (!message.reply_message)
+			return await message.sendMessage('*Reply to a message*')
+		const key = message.reply_message.key
+		if (!key.fromMe) {
+			const participants = await message.groupMetadata(message.jid)
+			const isImAdmin = await isAdmin(participants, message.client.user.jid)
+			if (!isImAdmin) return await message.sendMessage(`_I'm not admin._`)
+		}
+		return await message.sendMessage(key, {}, 'delete')
+	}
+)
