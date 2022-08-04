f2a350379086c377889cc48f8bdbaf00bd8a6761
LyFE <76509367+lyfe00011@users.noreply.github.com>
2022-08-04 09:28:36 +0530

    Update dlt.js

--- plugins/dlt.js
+++ plugins/dlt.js
@@ -11,7 +11,7 @@ bot(
 		if (!message.reply_message)
 			return await message.sendMessage('*Reply to a message*')
 		const key = message.reply_message.key
-		if (!key.fromMe) {
+		if (!key.fromMe && message.isGroup) {
 			const participants = await message.groupMetadata(message.jid)
 			const isImAdmin = await isAdmin(participants, message.client.user.jid)
 			if (!isImAdmin) return await message.sendMessage(`_I'm not admin._`)
