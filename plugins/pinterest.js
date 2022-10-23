ec77c6c3dafc499bba8106c10f12aa7f9fcfc6de
lyfe00011 <lyfe00011@gmail.com>
2022-10-23 11:05:40 +0530

    fix

--- plugins/pinterest.js
+++ plugins/pinterest.js
@@ -11,7 +11,7 @@ bot(
 		match = isUrl(match || message.reply_message.text)
 		if (!match) return await message.send('_Example : pinterest url_')
 		const result = await pinterest(match)
-		if (!result)
+		if (!result.length)
 			return await message.send('*Not found*', {
 				quoted: message.quoted,
 			})
