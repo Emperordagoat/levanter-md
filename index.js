528085491ec204f4ad12bbe115931fb25d8d8193
lyfe00011 <lyfe00011@gmail.com>
2022-12-08 21:41:08 +0530

    fix

--- index.js
+++ index.js
@@ -2,10 +2,10 @@ const { Client } = require('./lib/client')
 const { DATABASE, VERSION } = require('./config')
 const start = async () => {
 	try {
-		const bot = new Client()
 		console.log(`levanter ${VERSION}`)
 		await DATABASE.sync()
 		console.log('Database syncing...')
+		const bot = new Client()
 		await bot.connect()
 	} catch (error) {
 		console.error(error)
