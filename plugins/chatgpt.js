2a26c72b9b90dfe41bae567c1df1a658af474c83
lyfe00011 <lyfe00011@gmail.com>
2023-04-06 19:52:03 +0530

    add dall

--- plugins/chatgpt.js
+++ plugins/chatgpt.js
@@ -1,5 +1,5 @@
 const config = require('../config')
-const { bot, getGPTResponse } = require('../lib')
+const { bot, getGPTResponse, getDallEResponse } = require('../lib')
 
 bot(
 	{
@@ -19,5 +19,26 @@ bot(
 			)
 		const res = await getGPTResponse(match)
 		await message.send(res, { quoted: message.data })
+	}
+)
+
+bot(
+	{
+		pattern: 'dall ?(.*)',
+		fromMe: true,
+		desc: 'dall image generator',
+		type: 'AI',
+	},
+	async (message, match) => {
+		if (!config.GPT)
+			return await message.send(
+				'Get a key from https://platform.openai.com/account/api-keys\n\nsetvar GPT = your_key'
+			)
+		if (!match)
+			return await message.send(
+				'*Example : dall a close up, studio photographic portrait of a white siamese cat that looks curious, backlit ears*'
+			)
+		const res = await getDallEResponse(match)
+		await message.sendFromUrl(res)
 	}
 )
