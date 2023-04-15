d96d065d7cdefa1de12ca879e829bd901df25a40
lyfe00011 <lyfe00011@gmail.com>
2023-04-15 13:28:11 +0530

    fix

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 const { Client } = require('./lib/client')
 const { DATABASE, VERSION } = require('./config')
+const { stopInstance } = require('./lib/pm2')
+
 const start = async () => {
 	try {
 		logger.info(`levanter ${VERSION}`)
+		try {
+			await DATABASE.authenticate()
+		} catch (error) {
+			console.error('Unable to connect to the database:', error)
+			stopInstance()
+		}
 		await DATABASE.sync()
 		logger.info('Database syncing...')
 		const bot = new Client()
