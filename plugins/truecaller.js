905f97053208a7f8771d4a920aac64f8c63490aa
lyfe00011 <lyfe00011@gmail.com>
2023-10-23 21:13:21 +0530

    add truecaller

---
+++ plugins/truecaller.js
@@ -0,0 +1,27 @@
+const { bot, truecaller } = require('../lib/index')
+
+bot(
+  {
+    pattern: 'truecaller ?(.*)',
+    fromMe: true,
+    desc: 'search number in truecaller',
+    type: 'search',
+  },
+  async (message, match) => {
+    if (!match) return await message.send(`*Example :* truecaller 919876543210`)
+
+    const res = await truecaller.search(match)
+
+    if (res.message) {
+      return await message.send(res.message)
+    }
+    let msg = ''
+    if (res.name) msg += `*Name :* ${res.name}\n`
+    if (res.gender) msg += `*Gender :* ${res.gender}\n`
+    msg += `*Type :* ${res.numberType}(${res.type})\n`
+    msg += `*Carrier :* ${res.carrier}\n`
+    if (res.city) msg += `*City :* ${res.city}\n`
+    msg += `*DailingCode :* ${res.dialingCode}(${res.countryCode})`
+    await message.send(msg)
+  }
+)
