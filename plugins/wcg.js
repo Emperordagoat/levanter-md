1332076421ad2e270c5aa7c101bb518dcd6e2bca
lyfe00011 <lyfe00011@gmail.com>
2023-10-31 15:00:14 +0530

    add wrg

--- plugins/wcg.js
+++ plugins/wcg.js
@@ -5,7 +5,6 @@ bot(
     fromMe: true,
     desc: 'word chain game\nwcg start to force start game',
     type: 'game',
-    // onlyGroup: true,
   },
   async (message, match) => {
     if (match == 'start') {
@@ -14,6 +13,24 @@ bot(
     if (match == 'end') {
       return await wcg.end(message.jid, message.participant)
     }
-    wcg.start_game(message.jid, message.participant)
+    wcg.start_game(message.jid, message.participant, 'chain', match)
+  }
+)
+
+bot(
+  {
+    pattern: 'wrg ?(.*)',
+    fromMe: true,
+    desc: 'random word game\nwrg start to force start game',
+    type: 'game',
+  },
+  async (message, match) => {
+    if (match == 'start') {
+      return await wcg.start(message.jid, message.participant)
+    }
+    if (match == 'end') {
+      return await wcg.end(message.jid, message.participant)
+    }
+    wcg.start_game(message.jid, message.participant, 'random', match)
   }
 )
