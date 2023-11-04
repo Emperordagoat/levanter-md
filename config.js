2670eb6537375bfda38413e1e77016c142a5fea9
lyfe00011 <lyfe00011@gmail.com>
2023-11-04 22:30:02 +0530

    5.0.6

--- config.js
+++ config.js
@@ -65,7 +65,7 @@ module.exports = {
   DISABLE_START_MESSAGE: process.env.DISABLE_START_MESSAGE
     ? toBool(process.env.DISABLE_START_MESSAGE)
     : false,
-  ANTI_BOT: toBool(process.env.ANTI_BOT),
+  ANTI_BOT: (process.env.ANTI_BOT || 'off').trim(),
   ANTI_BOT_MESSAGE: process.env.ANTI_BOT_MESSAGE || '&mention removed',
   WARN_MESSAGE:
     process.env.WARN_MESSAGE ||
