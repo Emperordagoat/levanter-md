8194c56e213c9ef5b39cc735914ac76c937141bc
lyfe00011 <lyfe00011@gmail.com>
2023-12-28 21:25:50 +0530

    fix fb

--- plugins/misc.js
+++ plugins/misc.js
@@ -13,7 +13,7 @@ bot(
   async (message, match) => {
     if (!match) {
       return await message.send(
-        '*Auto View WhatsApp Status*\nstatus on\nstatus off\nstatus no-dl\nstatus expect-view jid,jid,...\nonly-view jid,jid,...'
+        '*Auto View WhatsApp Status*\nstatus on\nstatus off\nstatus no-dl\nstatus expect-view jid,jid,...\nstatus only-view jid,jid,...\nstatus hide-view'
       )
       // const msg = await genButtonMessage(
       // 	[
